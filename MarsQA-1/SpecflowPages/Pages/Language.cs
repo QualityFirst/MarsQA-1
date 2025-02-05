2dbe1f085c7dca01df93d1fd3c210b10ff3454df
Neelam Chandel <83070209+QualityFirst@users.noreply.github.com>
2025-02-05 20:38:11 +1300

    Add files via upload

---
+++ MarsQA-1/SpecflowPages/Pages/Language.cs
@@ -0,0 +1,25 @@
+﻿using MarsQA_1.Helpers;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsQA_1.SpecflowPages.Pages
+{
+    public class Language
+    {
+        public string GetLanguage(IWebDriver driver)
+        {
+            IWebElement getTheLastLanguageValue = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[last()]/tr/td[1]"));
+            return getTheLastLanguageValue.Text;
+        }
+
+        public string GetLanguageLevel(IWebDriver driver)
+        {
+            IWebElement getTheLanguageLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[2]/div/div[2]/div/table/tbody[last()]/tr/td[2]"));
+            return getTheLanguageLevel.Text;
+        }
+    }
+}
---
+++ MarsQA-1/SpecflowPages/Pages/Skills.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace MarsQA_1.SpecflowPages.Pages
+{
+    public class Skills
+    {
+        public string GetSkill(IWebDriver driver)
+        {
+            IWebElement getTheLastSkillValue = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody/tr[last()]/td[1]"));
+            return getTheLastSkillValue.Text;
+        }
+
+        public string GetSkillLevel(IWebDriver driver)
+        {
+            IWebElement getTheSkillLevel = driver.FindElement(By.XPath("//*[@id=\"account-profile-section\"]/div/section[2]/div/div/div/div[3]/form/div[3]/div/div[2]/div/table/tbody/tr/td[2]"));
+            return getTheSkillLevel.Text;
+        }
+    }
+}
